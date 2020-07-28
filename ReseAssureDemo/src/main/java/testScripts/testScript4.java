25b100f78d1568fc479c7e3d70210dfaed34a2c7
DELL <DELL@DESKTOP-O8A2NA2>
2020-07-28 21:56:14 +0530

    test comit - 003

---
+++ ReseAssureDemo/src/main/java/testScripts/testScript4.java
@@ -0,0 +1,13 @@
+package testScripts;
+
+import org.testng.annotations.Test;
+
+public class testScript4 {
+	
+	 @Test
+	 public void test04()
+	 {
+		 System.out.println("This is branch 001 data- merge req 3");
+	 }
+
+}
