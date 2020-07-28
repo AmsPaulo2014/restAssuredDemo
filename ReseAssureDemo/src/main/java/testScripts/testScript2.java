466063b07c46350110fae09d0fdada8fa28304f1
DELL <DELL@DESKTOP-O8A2NA2>
2020-07-28 21:39:19 +0530

    this is branch001 commit and push

---
+++ ReseAssureDemo/src/main/java/testScripts/testScript2.java
@@ -0,0 +1,13 @@
+package testScripts;
+
+import org.testng.annotations.Test;
+
+public class testScript2 {
+
+ @Test
+ public void test02()
+ {
+	 System.out.println("This is branch 001 data");
+ }
+
+}
