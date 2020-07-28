fedc08f311f79a3d4a3e908d2d7de46a10778bd2
DELL <DELL@DESKTOP-O8A2NA2>
2020-07-28 20:38:33 +0530

    First commit

---
+++ ReseAssureDemo/src/main/java/testScripts/testScript1.java
@@ -0,0 +1,13 @@
+package testScripts;
+
+import org.testng.annotations.Test;
+
+public class testScript1 {
+	
+	@Test
+	public void test001()
+	{
+		System.out.println("This is test script 1");
+	}
+
+}
